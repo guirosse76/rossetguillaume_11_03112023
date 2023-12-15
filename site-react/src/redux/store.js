528f8ad3fe740f8494b069f76520c7d44cb8a62c
Guillaume <rossetguillaume759@gmail.com>
2023-12-15 16:41:22 +0100

    Update store.js

--- site-react/src/redux/store.js
+++ site-react/src/redux/store.js
@@ -1,14 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import firstNameReducer from "./features/User/firstName";
-// import lastNameReducer from "./features/User/lastName";
-// import tokenReducer from "./features/Token/token";
+
 import userReducer from "./features/user";
 
 export const store = configureStore({
   reducer: {
-    // firstName: firstNameReducer,
-    // lastName: lastNameReducer,
-    // token: tokenReducer,
     user: userReducer,
   },
 });
